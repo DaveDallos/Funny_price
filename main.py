ffd94f988b9ec21cacdd1e7dbad1279537fa0bf0
bubsl <bubslite@yandex.ru>
2022-04-12 20:03:42 +0300

    New product, add page navigation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for
-import os
 
 app = Flask(__name__)
 
@@ -11,10 +10,14 @@ def index():
     v3090 = "static/img/3090.jpg"
     v3080 = "static/img/3080.jpg"
     v1660super = "static/img/1660 SUPER.jpg"
+    v3050 = "static/img/3050.jpg"
+    v2060 = "static/img/2060.jpg"
+    v6700xt = "static/img/6700 XT.jpg"
+    v6800xt = "static/img/6800 XT.jpg"
     znachok = "static/img/znachok.png"
     return render_template('up_menu.html', title='Смешные цены',
                            v3070ti=v3070ti, v3090=v3090, v3080=v3080, v1660super=v1660super,
-                           znachok=znachok)
+                           znachok=znachok, v3050=v3050, v2060=v2060, v6700xt=v6700xt, v6800xt=v6800xt)
 
 
 @app.route('/cart')
